238d040cd531c279d7fca1f8f5f4d97934dd67f6
Roberto Pacheco <robertopvc@gmail.com>
2022-06-27 22:19:27 -0300

    testing instantiate ee-dnn

--- utils.py
+++ utils.py
@@ -73,8 +73,8 @@ class LoadDataset():
 		test_data = torch.utils.data.Subset(test_set, indices=test_idx)
 
 		train_loader = torch.utils.data.DataLoader(train_data, batch_size=self.batch_size_train, shuffle=True, num_workers=4)
-		val_loader = torch.utils.data.DataLoader(val_data, batch_size=self.batch_size_test, num_workers=4)
-		test_loader = torch.utils.data.DataLoader(test_data, batch_size=self.batch_size_test, num_workers=4)
+		val_loader = torch.utils.data.DataLoader(val_data, batch_size=1, num_workers=4)
+		test_loader = torch.utils.data.DataLoader(test_data, batch_size=1, num_workers=4)
 
 		return train_loader, val_loader, val_loader
 
