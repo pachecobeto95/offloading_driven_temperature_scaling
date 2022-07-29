3ab91ae42b68c268ea915b09732b14e06e853cf4
Roberto Pacheco <robertopvc@gmail.com>
2022-07-29 13:57:02 -0300

    a

--- offloading_drive_temperature_scaling.py
+++ offloading_drive_temperature_scaling.py
@@ -34,7 +34,7 @@ def main(args):
 	#Load the trained early-exit DNN model.
 	ee_model = ee_model.to(device)
 	ee_model.load_state_dict(torch.load(model_path, map_location=device)["model_state_dict"])
-
+	sys.exit()
 	# Obtain the confidences and predictions running an early-exit DNN inference. It returns as a Dataframe
 	df_preds = utils.eval_ee_dnn_inference(test_loader, ee_model, args.n_branches, device, inference_data_path, args.read_inf_data)
 	
