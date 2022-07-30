c5b67ad3f44c9d83d61f8260db8e916d1a2013f6
Roberto Pacheco <robertopvc@gmail.com>
2022-07-30 17:29:08 -0300

    testing spsa of inference time

--- spsa.py
+++ spsa.py
@@ -306,11 +306,9 @@ def compute_avg_inference_time(temp_list, n_branches, threshold, df, inf_time_br
 		else:
 			confs = remaining_data["conf_branch_%s"%(i+1)]
 			calib_confs = confs/temp_list[i]
-			print(calib_confs)
-			sys.exit()
 			early_exit_samples = calib_confs >= threshold
 
-		print(len(calib_confs >= threshold), current_n_samples)
+		print(sum(calib_confs >= threshold), current_n_samples)
 		numexits = remaining_data[early_exit_samples]["conf_branch_%s"%(i+1)].count()
 
 		prob = numexits/current_n_samples
