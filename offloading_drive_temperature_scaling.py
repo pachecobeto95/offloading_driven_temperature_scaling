62cc4aef802775175385ff5ef897b6bdf5048c8a
Roberto Pacheco <robertopvc@gmail.com>
2022-08-09 10:17:59 -0300

    a

--- offloading_drive_temperature_scaling.py
+++ offloading_drive_temperature_scaling.py
@@ -41,7 +41,7 @@ def main(args):
 	# Obtain the average inference time to process up to each side branch.
 	inference_time_branch = utils.collect_avg_inference_time_branch(ee_model, test_loader, args.n_branches, args.threshold, device)
 	print(inference_time_branch)
-	inf_time_measure = spsa.measure_inference_time(np.ones(args.n_branches+1), args.n_branches, args.threshold, test_loader, ee_model, device)
+	inf_time_measure = spsa.measure_inference_time([0.0001, 1, 1, 1, 1, 1], args.n_branches, args.threshold, test_loader, ee_model, device)
 	print(inf_time_measure)
 	sys.exit()
 	#theta_opt_acc, loss_opt_acc = spsa.run_SPSA_accuracy(ee_model, df_preds, args.threshold, args.max_iter, args.n_branches, args.a0, 
