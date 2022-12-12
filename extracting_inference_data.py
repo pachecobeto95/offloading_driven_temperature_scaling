70e171f8e04f5ed0946d7b561a07fe17fafd8ba9
Roberto Pacheco <robertopvc@gmail.com>
2022-12-12 14:51:58 -0300

    a

---
+++ extracting_inference_data.py
@@ -0,0 +1,137 @@
+import os, time, sys, json, os, argparse, torch
+import config, utils, spsa
+from early_exit_dnn import Early_Exit_DNN
+import numpy as np
+import pandas as pd
+
+
+
+def main(args):
+
+
+	model_id = config.models_id_dict[args.model_name]
+
+	n_classes = config.nr_class_dict[args.dataset_name]
+
+	device = torch.device('cuda' if (torch.cuda.is_available() and args.cuda) else 'cpu')
+
+	model_path = os.path.join(config.DIR_NAME, "models", args.model_name, "models", 
+		"ee_mobilenet_branches_%s_id_%s.pth"%(args.n_branches, model_id))
+
+	inf_data_path = os.path.join(config.DIR_NAME, "models", args.model_name, "results", 
+		"inference_data_%s.csv"%(model_id))
+
+	inf_time_path = os.path.join(config.DIR_NAME, "models", args.model_name, "results", 
+		"inference_time_%s.csv"%(model_id))
+
+	# Instantiate LoadDataset class
+	dataset = utils.LoadDataset(args, model_id)
+
+	dataset_path = config.dataset_path_dict[args.dataset_name]
+	idx_path = config.idx_path_dict[args.dataset_name]
+
+	_, _, test_loader = dataset.getDataset(dataset_path, args.dataset_name, idx_path)
+
+	#Instantiate the Early-exit DNN model.
+	ee_model = Early_Exit_DNN(args.model_name, n_classes, args.pretrained, args.n_branches, args.input_dim, 
+		args.exit_type, device, args.distribution)
+	#Load the trained early-exit DNN model.
+	ee_model = ee_model.to(device)
+	ee_model.load_state_dict(torch.load(model_path, map_location=device)["model_state_dict"])
+
+	# Obtain the confidences and predictions running an early-exit DNN inference. It returns as a Dataframe
+	df_inference_data = utils.run_ee_dnn_inference(test_loader, model, n_branches, device)
+
+	# Obtain the average inference time to process up to each side branch.
+	df_inf_time_branches = utils.collect_avg_inference_time_branch(ee_model, test_loader, args.n_branches, 
+		args.threshold, device)
+
+	df_inference_data.to_csv(inf_data_path)
+
+	df_inf_time_branches.to_csv(inf_time_path)
+
+
+
+if (__name__ == "__main__"):
+	# Input Arguments to configure the early-exit model .
+	parser = argparse.ArgumentParser(description="Learning the Temperature driven for offloading.")
+
+	#We here insert the argument dataset_name. 
+	#The initial idea is this novel calibration method evaluates three dataset for image classification: cifar10, cifar100 and
+	#caltech256. First, we implement caltech256 dataset.
+	parser.add_argument('--dataset_name', type=str, default=config.dataset_name, 
+		choices=["caltech256"], help='Dataset name (default: Caltech-256)')
+
+	#We here insert the argument model_name. 
+	#We evalue our novel calibration method Offloading-driven Temperature Scaling in four early-exit DNN:
+	#MobileNet, ResNet18, ResNet152, VGG16
+	parser.add_argument('--model_name', type=str, choices=["mobilenet", "resnet18", "resnet152", "vgg16"], 
+		help='DNN model name (default: mobilenet)')
+
+	#This argument defines the ratio to split the Traning Set, Val Set, and Test Set.
+	parser.add_argument('--split_ratio', type=float, default=config.split_ratio, help='Split Ratio')
+
+	#This argument defined the batch sizes. 
+	parser.add_argument('--batch_size_train', type=int, default=config.batch_size_train, 
+		help='Train Batch Size. Default: %s'%(config.batch_size_train))
+	parser.add_argument('--batch_size_test', type=int, default=config.batch_size_test, 
+		help='Test Batch Size. Default: %s'%(config.batch_size_test))
+
+	# This argument defines the input dimension.
+	parser.add_argument('--input_dim', type=int, default=config.input_dim, 
+		help='Input Dimension. Default: %s'%(config.input_dim))
+
+	# This argument defines the seed for random operations.
+	parser.add_argument('--seed', type=int, default=config.seed, 
+		help='Seed. Default: %s'%(config.seed))
+
+	# This argument defines the backbone DNN is pretrained.
+	parser.add_argument('--pretrained', type=bool, default=config.pretrained, 
+		help='Is backbone DNN pretrained? Default: %s'%(config.pretrained))
+
+	# This argument defines Offloading-drive TS uses GPU board.
+	parser.add_argument('--cuda', type=bool, default=config.cuda, 
+		help='Cuda? Default: %s'%(config.cuda))
+
+	parser.add_argument('--exit_type', type=str, default=config.exit_type, 
+		help='Exit Type. Default: %s'%(config.exit_type))
+
+	parser.add_argument('--distribution', type=str, default=config.distribution, 
+		help='Distribution. Default: %s'%(config.distribution))
+
+	parser.add_argument('--n_branches', type=int, default=config.n_branches, 
+		help='Number of side branches. Default: %s'%(config.n_branches))
+
+	parser.add_argument('--max_iter', type=int, default=config.max_iter, 
+		help='Number of epochs. Default: %s'%(config.max_iter))
+
+	parser.add_argument('--read_inf_data', type=bool, default=config.read_inf_data, 
+		help='Do you read inference data. Default: %s'%(config.read_inf_data))
+
+	parser.add_argument('--a0', type=int, default=config.a0, 
+		help='a0. Default: %s'%(config.a0))
+
+	parser.add_argument('--c', type=int, default=config.c, 
+		help='c. Default: %s'%(config.c))
+
+	parser.add_argument('--alpha', type=int, default=config.alpha, 
+		help='alpha. Default: %s'%(config.alpha))
+
+	parser.add_argument('--gamma', type=int, default=config.gamma, 
+		help='gamma. Default: %s'%(config.gamma))
+
+	parser.add_argument('--threshold', type=float, default=config.threshold, 
+		help="Threshold that decides if the prediction if confidence enough. Default: %s"%(config.threshold))	
+
+	parser.add_argument('--step', type=float, default=config.step, 
+		help="Step of beta. Default: %s"%(config.step))	
+
+	args = parser.parse_args()
+
+	main(args)
+
+
+
+
+
+
