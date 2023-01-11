0b9fccb40c9ac85a577a8ba934122cdf0592309c
Roberto Pacheco <robertopvc@gmail.com>
2023-01-11 17:49:19 -0300

    learning again

--- utils.py
+++ utils.py
@@ -48,6 +48,7 @@ def load_caltech256(args, dataset_path, save_indices_path, input_dim, dim):
 		train_idx = np.load(train_idx_path)
 		val_idx = np.load(val_idx_path)
 		test_idx = np.load(test_idx_path, allow_pickle=True)
+		print(test_idx)
 
 	else:
 		# This line get the indices of the samples which belong to the training dataset and test dataset. 
