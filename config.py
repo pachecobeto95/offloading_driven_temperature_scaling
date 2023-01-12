e63b47c3c922530aa04eb0754e0caaa217588abf
Roberto Pacheco <robertopvc@gmail.com>
2023-01-12 09:16:35 -0300

    a

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ resultsPath = os.path.join(DIR_NAME, "results")
 dataset_path_dict = {"caltech256": os.path.join(root_dataset_path, "caltech256")}
 idx_path_dict = {"caltech256": os.path.join(root_indices_path, "caltech256")}
 nr_class_dict = {"caltech256": {3: 258, 5: 257, 1: 257}}
-input_dim_dict = {3: [330, 300], 5: [256, 224]}
+input_dim_dict = {3: [330, 300], 5: [256, 224], 1: [256, 224]}
 
 filePath_acc = os.path.join(resultsPath, "temp_scaling_accuracy.csv")
 filePath_inf_time = os.path.join(resultsPath, "temp_scaling_inference_time.csv")
