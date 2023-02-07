1c9f6ef7ef1831695697d005da33dc27b50724a6
Roberto Pacheco <robertopvc@gmail.com>
2023-02-07 12:33:41 -0300

    a

--- spsa.py
+++ spsa.py
@@ -66,7 +66,7 @@ class SPSA (object):
 	def init_hyperparameters(self):
 
 		# A is <= 10% of the number of iterations
-		A = self.nr_iter*0.2
+		A = self.nr_iter*1
 
 		# order of magnitude of first gradients
 		#magnitude_g0 = np.abs(self.grad(self.function, self.theta_initial, self.c).mean())
