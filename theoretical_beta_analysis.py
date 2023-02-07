21887277d83b0a9fd785988e236c329a1e081a69
Roberto Pacheco <robertopvc@gmail.com>
2023-02-07 12:44:41 -0300

    a

--- theoretical_beta_analysis.py
+++ theoretical_beta_analysis.py
@@ -64,7 +64,7 @@ def main(args):
 
 	resultsPath = os.path.join(config.DIR_NAME, "theoretical_beta_analysis_%s_%s_branches_%s_with_overhead.csv"%(args.model_name, args.n_branches, args.model_id))
 
-	threshold_list = [0.7, 0.8, 0.9]
+	threshold_list = [0.8, 0.9]
 
 	beta_list = np.round(np.arange(1, -0.01, -0.01), 2)
 
