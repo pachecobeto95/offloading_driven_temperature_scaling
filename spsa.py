3f2fa2b3d0e572b9f83c8f87899506c9e9c8a631
Roberto Pacheco <robertopvc@gmail.com>
2023-02-07 12:46:23 -0300

    a

--- spsa.py
+++ spsa.py
@@ -512,7 +512,7 @@ def run_theoretical_beta_opt(df_inf_data, beta, opt_acc, opt_inf_time, threshold
 
 	max_exits = max_branches + 1
 
-	theta_initial, min_bounds = np.ones(n_branches_edge), np.zeros(n_branches_edge)
+	theta_initial, min_bounds = 1.5*np.ones(n_branches_edge), np.zeros(n_branches_edge)
 
 	# Instantiate SPSA class to initializes the parameters
 	optim = SPSA(theoretical_beta_function, theta_initial, max_iter, n_branches_edge, a0, c, alpha, gamma, min_bounds, 
