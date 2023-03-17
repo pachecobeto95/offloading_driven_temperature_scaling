82b5573b888510251de67a28712800f6cbea9071
Roberto Pacheco <robertopvc@gmail.com>
2023-03-17 08:58:23 -0300

    a

--- theoretical_beta_analysis.py
+++ theoretical_beta_analysis.py
@@ -122,7 +122,7 @@ def main(args):
 	global_ts_path = os.path.join(config.DIR_NAME, "alternative_temperature_%s_%s_branches_id_%s.csv"%(args.model_name, args.n_branches, args.model_id))
 
 
-	threshold_list = [0.8, 0.9]
+	threshold_list = [0.8]
 
 	beta_list = np.round(np.arange(1, -0.01, -0.01), 2)
 
