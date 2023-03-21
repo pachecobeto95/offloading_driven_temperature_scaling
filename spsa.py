db362d98ee827fe4eab63a8a6c92c6c8b4f4fbcb
Roberto Pacheco <robertopvc@gmail.com>
2023-03-21 16:12:59 -0300

    a

--- spsa.py
+++ spsa.py
@@ -51,7 +51,7 @@ class SPSA (object):
 		self.a0 = a0
 		self.alpha = alpha
 		self.gamma = gamma
-		self.c = 0.5 # a small number
+		self.c = 0.1 # a small number
 		self.min_bounds = min_bounds
 		self.args = args
 		self.ens_size = ens_size
