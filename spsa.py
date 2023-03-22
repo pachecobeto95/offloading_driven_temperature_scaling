246549577c634de9a1901fe2416bd6855a40e7dd
Roberto Pacheco <robertopvc@gmail.com>
2023-03-22 16:11:40 -0300

    a

--- spsa.py
+++ spsa.py
@@ -400,6 +400,7 @@ def compute_reliability_diagram(df, pdf, confs, idx_branch, temp_list, delta_ste
 	bin_uppers = bin_boundaries[1:]
 	conf_list, acc_list = [], [] 
 	print(df.columns)
+	sys.exit()
 
 	bin_size = 1/n_bins
 	positions = np.arange(0+bin_size/2, 1+bin_size/2, bin_size)
