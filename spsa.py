95165567b3d5e813f565c514cfa010dbd2610310
Roberto Pacheco <robertopvc@gmail.com>
2023-03-22 09:43:30 -0300

    a

--- spsa.py
+++ spsa.py
@@ -351,7 +351,10 @@ def compute_prob_success_branch(temp_list, idx_branch, threshold, df):
 	data_conf = np.float64(data_conf)
 	data_conf = data_conf[data_conf < 1E308]
 	data_conf = data_conf[:, np.newaxis]
-	print(data_conf)
+
+	if(len(data_conf) == 0):
+		break
+
 
 	conf_d = np.linspace(threshold, 1, 100)
 	conf_col = conf_d[:, np.newaxis]
