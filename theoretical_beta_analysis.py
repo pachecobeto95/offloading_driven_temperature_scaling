137057ed690043c9f1ce317a488d64d85c13af6d
Roberto Pacheco <robertopvc@gmail.com>
2023-04-20 15:06:29 -0300

    a

--- theoretical_beta_analysis.py
+++ theoretical_beta_analysis.py
@@ -116,7 +116,7 @@ def main(args):
 
 	#temp_data_path = os.path.join(config.DIR_NAME, "temperature_%s_%s_branches_id_%s.csv"%(args.model_name, args.n_branches, args.model_id))
 
-	resultsPath = os.path.join(config.DIR_NAME, "theoretical_beta_analysis_%s_%s_branches_%s_with_overhead_with_nano_with_test_set_pos_1_review_exp.csv"%(args.model_name, args.n_branches, args.model_id))
+	resultsPath = os.path.join(config.DIR_NAME, "theoretical_beta_analysis_%s_%s_branches_%s_with_overhead_with_nano_with_test_set_pos_1_review_theo.csv"%(args.model_name, args.n_branches, args.model_id))
 
 	#global_ts_path = os.path.join(config.DIR_NAME, "alternative_temperature_%s_%s_branches_id_%s.csv"%(args.model_name, args.n_branches, args.model_id))
 
