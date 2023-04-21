c683c09e862a73f721450b3a235758c4f4b5b9e3
Roberto Pacheco <robertopvc@gmail.com>
2023-04-21 10:32:45 -0300

    calibrating multi exits

--- calibrating_global_ts_multi_branches.py
+++ calibrating_global_ts_multi_branches.py
@@ -6,54 +6,32 @@ import temperature_scaling, ee_nn
 
 def main(args):
 
-	n_classes = 257
 
-	device = torch.device('cuda' if (torch.cuda.is_available() and args.cuda) else 'cpu')
+	n_classes, input_dim, dim = 257, 330, 300
 
-	model_path = os.path.join(config.DIR_NAME, "new_models", "models", "ee_%s_%s_branches_id_%s.pth"%(args.model_name, args.n_branches, args.model_id) )	
+	temp_path = os.path.join(config.DIR_NAME, "alternative_temperature_%s_%s_branches_id_%s.csv"%(args.model_name, args.n_branches, args.model_id))
 
-	multi_branch_model_path = os.path.join(config.DIR_NAME, "new_models", "models", "ee_%s_%s_branches_id_%s.pth"%(args.model_name, args.n_branches, args.model_id) )	
+	model_path = os.path.join(config.DIR_NAME, "new_models", "models", "ee_%s_%s_branches_id_%s.pth"%(config.model_name, args.n_branches, args.model_id))
 
 	dataset_path = config.dataset_path_dict[args.dataset_name]
-
-	temp_savePath = os.path.join(config.DIR_NAME, "alternative_temperature_%s_%s_branches_id_%s2.csv"%(args.model_name, args.n_branches, args.model_id) )
-
-	#inf_data_path = os.path.join(config.DIR_NAME, "new_inference_data", "inference_data_%s_%s_branches_%s_local_server.csv"%(args.model_name, args.n_branches, args.model_id))
-
-	indices_path = os.path.join(config.DIR_NAME, "indices", "caltech256", "validation_idx_caltech256_id_1.npy")
+	
+	device = torch.device('cuda' if (torch.cuda.is_available() and args.cuda) else 'cpu')
 
 	model_dict = torch.load(model_path, map_location=device)
-	multi_model_dict = torch.load(multi_branch_model_path, map_location=device)
 
-	val_idx, test_idx = model_dict["test"], model_dict["test"]
+	test_idx, val_idx = model_dict["test"], model_dict["val"]
 
-	val_idx = np.load(indices_path)
-
-	#Load Early-exit DNN model.	
 	ee_model = ee_nn.Early_Exit_DNN(args.model_name, n_classes, args.pretrained, args.n_branches, args.dim, device, args.exit_type, args.distribution)
-	ee_model.load_state_dict(multi_model_dict["model_state_dict"])
-	ee_model = 	ee_model.to(device)
+	ee_model.load_state_dict(model_dict["model_state_dict"])
+	ee_model = ee_model.to(device)
 	ee_model.eval()
 
 	#Load Dataset 
-	val_loader = utils.load_caltech256_test_inference(args, dataset_path, val_idx)
+	test_loader = utils.load_caltech256_test_inference(args, dataset_path, test_idx)
 
-	theta_initial = 1.5
 	threshold_list = [0.7, 0.8, 0.9]
 
-	for threshold in threshold_list:
-		# Instantiate SPSA class to initializes the parameters
-		global_ts = temperature_scaling.GlobalTemperatureScaling(ee_model, device, theta_initial, args.max_iter, args.n_branches, threshold)
-
-		global_ts.run(val_loader)
-
-		#temperature_overall = [global_ts.temperature_overall.item()]*args.n_branches
-
-		result = {"threshold": threshold, "n_branches": args.n_branches, "calib_mode": "global_TS", "temperature": global_ts.temperature_overall.item()}
-
-		df = pd.DataFrame([result])
-		df.to_csv(temp_savePath, mode='a', header=not os.path.exists(temp_savePath))
-
+	
 
 
 
@@ -72,9 +50,6 @@ if (__name__ == "__main__"):
 	#MobileNet, ResNet18, ResNet152, VGG16
 	parser.add_argument('--model_name', type=str, choices=["mobilenet", "resnet18", "resnet152", "vgg16"], 
 		help='DNN model name (default: mobilenet)')
-
-	#This argument defines the ratio to split the Traning Set, Val Set, and Test Set.
-	parser.add_argument('--split_ratio', type=float, default=config.split_ratio, help='Split Ratio')
 
 	# This argument defines the seed for random operations.
 	parser.add_argument('--seed', type=int, default=config.seed, 
@@ -102,12 +77,10 @@ if (__name__ == "__main__"):
 
 	parser.add_argument('--model_id', type=int, default=1)	
 
-	parser.add_argument('--input_dim', type=int, default=330, help='Input Dim. Default: %s'%config.input_dim)
+	parser.add_argument('--input_dim', type=int, default=330)
 
 	parser.add_argument('--dim', type=int, default=300, help='Dim. Default: %s')
-
 
 	args = parser.parse_args()
 
 	main(args)
-
