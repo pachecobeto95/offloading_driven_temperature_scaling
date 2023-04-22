2f48a77ab9e04f87cee9fc2144a28ed5fbfc0adf
Roberto Pacheco <robertopvc@gmail.com>
2023-04-21 23:45:40 -0300

    a

--- spsa.py
+++ spsa.py
@@ -249,7 +249,9 @@ def theoretical_beta_function(temp_list, n_branches, max_exits, threshold, df, d
 
 	#inf_time_current, _ = compute_inference_time(temp_list, n_branches, max_exits, threshold, df, df_device, overhead)
 	inf_time_current, _ = compute_inference_time_multi_branches(temp_list, n_branches, max_exits, threshold, df, df_device, overhead)
+	inf_time_current2, _ = compute_inference_time_multi_branches2(temp_list, n_branches, max_exits, threshold, df, df_device, overhead)
 
+	print(inf_time_current, inf_time_current2)
 	#f = beta*acc_current + (1-beta)*inf_time_current 
 	#f = (1-beta)*inf_time_current - beta*acc_current
 	f = inf_time_current - beta*acc_current
@@ -268,7 +270,7 @@ def beta_function(temp_list, n_branches, max_exits, threshold, df, df_device, lo
 
 	return f, ee_prob
 
-
+#'delta_inferente_time_branch_2',
 
 
 def compute_inference_time_multi_branches2(temp_list, n_branches, max_exits, threshold, df, df_device, overhead):
@@ -291,16 +293,11 @@ def compute_inference_time_multi_branches2(temp_list, n_branches, max_exits, thr
 		n_exit_branch = remaining_data[early_exit_samples]["conf_branch_%s"%(i+1)].count()
 		n_exits_device_list.append(n_exit_branch)
 
-		inf_time_branch_device = df_device["inferente_time_branch_%s"%(i+1)].mean()
+		inf_time_branch_device = df_device["delta_inferente_time_branch_%s"%(i+1)].mean()
 
-		#delta_inference_time = inf_time_branch_device - inf_time_previous_branch
-
-		#print(n_exit_branch, delta_inference_time)
-
-		avg_inference_time += n_exit_branch*inf_time_branch_device
+		avg_inference_time += n_remaining_samples*inf_time_branch_device
 
 		n_remaining_samples -= n_exit_branch
-		inf_time_previous_branch = inf_time_branch_device
 
 		remaining_data = remaining_data[~early_exit_samples]
 
@@ -308,7 +305,7 @@ def compute_inference_time_multi_branches2(temp_list, n_branches, max_exits, thr
 
 	inf_time_branch_cloud = df["inferente_time_branch_%s"%(n_branches+1)].mean()-df["inferente_time_branch_%s"%(n_branches)].mean()
 
-	avg_inference_time += n_remaining_samples*(df_device["inferente_time_branch_%s"%(n_branches)].mean()+overhead+inf_time_branch_cloud)
+	avg_inference_time += n_remaining_samples*(overhead+inf_time_branch_cloud)
 
 	avg_inference_time = avg_inference_time/float(n_samples)
 	early_classification_prob = sum(n_exits_device_list)/float(n_samples)
