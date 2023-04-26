50d4ce2b3e4e159e0a593e6865eb53fea0fb731d
Roberto Pacheco <robertopvc@gmail.com>
2023-04-26 18:16:05 -0300

    a

--- spsa.py
+++ spsa.py
@@ -76,7 +76,7 @@ class SPSA (object):
 		# the number 2 in the front is an estimative of
 		# the initial changes of the parameters,
 		# different changes might need other choices
-		a = 0.01*((A+1)**self.alpha)/magnitude_g0
+		a = ((A+1)**self.alpha)/magnitude_g0
 
 		return a, A, self.c
 
