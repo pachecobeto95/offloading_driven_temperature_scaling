dcebfe67140f8b829ac776f76d82316f12d536d2
pachecobeto95 <robertopvc@gmail.com>
2023-05-11 12:02:06 -0300

    a

--- plotAdaTSTradeoff.py
+++ plotAdaTSTradeoff.py
@@ -8,9 +8,9 @@ def plotBetaTradeOff(args, df_spsa, df_spsa1, df_no_calib, df_ts, threshold, n_b
 	acc_beta, inf_time_beta = df_spsa.beta_acc.values, df_spsa.beta_inf_time.values
 
 	acc_beta, inf_time_beta = df_spsa1[df_spsa1.beta_acc>0.6].beta_acc.values, df_spsa1[df_spsa1.beta_acc>0.6].beta_inf_time.values
-	acc_beta1, inf_time_beta1 = df_spsa1[df_spsa1.beta_acc>0.94].beta_acc.values, df_spsa1[df_spsa1.beta_acc>0.94].beta_inf_time.values
+	#acc_beta1, inf_time_beta1 = df_spsa1[df_spsa1.beta_acc>0.94].beta_acc.values, df_spsa1[df_spsa1.beta_acc>0.94].beta_inf_time.values
 
-	acc_beta, inf_time_beta = np.concatenate((acc_beta, acc_beta1)), np.concatenate((inf_time_beta, inf_time_beta1))
+	#acc_beta, inf_time_beta = np.concatenate((acc_beta, acc_beta1)), np.concatenate((inf_time_beta, inf_time_beta1))
 	
 	acc_beta_index = np.argsort(acc_beta)
 	acc_beta, inf_time_beta	= acc_beta[acc_beta_index], inf_time_beta[acc_beta_index]
