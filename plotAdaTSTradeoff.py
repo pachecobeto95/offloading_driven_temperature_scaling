2576b35d597e17ccf46754c72ba28cf7e20a6700
pachecobeto95 <robertopvc@gmail.com>
2023-05-11 11:25:20 -0300

    a

--- plotAdaTSTradeoff.py
+++ plotAdaTSTradeoff.py
@@ -5,7 +5,7 @@ import os, sys, argparse
 
 def plotBetaTradeOff(args, df_spsa, df_no_calib, df_ts, threshold, n_branches, overhead, plotPath):
 	print("oi")
-	acc_beta, inf_time_beta = -df_beta.beta_acc.values, df_beta.beta_inf_time.values
+	acc_beta, inf_time_beta = -df_spsa.beta_acc.values, df_spsa.beta_inf_time.values
 
 	print(acc_beta)
 
