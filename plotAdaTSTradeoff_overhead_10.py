e84b899579353ee754037acd073b5bbf774bad31
Roberto Pacheco <robertopvc@gmail.com>
2023-05-12 11:26:54 -0300

    a

--- plotAdaTSTradeoff_overhead_10.py
+++ plotAdaTSTradeoff_overhead_10.py
@@ -58,6 +58,8 @@ def main(args):
 	df_spsa, df_no_calib, df_ts = df_inf_data[df_inf_data.calib_mode=="beta_calib"], df_inf_data[df_inf_data.calib_mode=="no_calib"], df_inf_data[df_inf_data.calib_mode=="global_TS"]
 	df_spsa1, df_no_calib1, df_ts1 = df_inf_data1[df_inf_data1.calib_mode=="beta_calib"], df_inf_data1[df_inf_data1.calib_mode=="no_calib"], df_inf_data1[df_inf_data1.calib_mode=="global_TS"]
 
+	print(df_no_calib, df_ts)
+
 	plotPath = os.path.join(plotDir, "beta_analysis_%s_branches_threshold_%s_overhead_%s_with_nano"%(args.n_branches, threshold, args.overhead) )
 
 	plotBetaTradeOff(args, df_spsa, df_spsa1, df_no_calib, df_ts, threshold, args.n_branches, args.overhead, plotPath)
