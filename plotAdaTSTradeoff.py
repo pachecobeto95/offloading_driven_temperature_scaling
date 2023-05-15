428a1d9824a660bc08584c334c9d6c75bd136ca9
Roberto Pacheco <robertopvc@gmail.com>
2023-05-15 09:11:11 -0300

    a

--- plotAdaTSTradeoff.py
+++ plotAdaTSTradeoff.py
@@ -3,19 +3,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os, sys, argparse
 
-def plotBetaTradeOff(args, df_spsa, df_spsa1, df_no_calib, df_ts, threshold, n_branches, overhead, plotPath):
+#def plotBetaTradeOff(args, df_spsa, df_spsa1, df_no_calib, df_ts, threshold, n_branches, overhead, plotPath):
+def plotBetaTradeOff(args, df_spsa, df_no_calib, df_ts, threshold, n_branches, overhead, plotPath):
 	acc_beta, inf_time_beta = df_spsa.beta_acc.values, df_spsa.beta_inf_time.values
 
 	acc_beta, inf_time_beta = df_spsa[df_spsa.beta_acc>0.6].beta_acc.values, df_spsa[df_spsa.beta_acc>0.6].beta_inf_time.values
-	acc_beta1, inf_time_beta1 = df_spsa1[df_spsa1.beta_acc>0.6].beta_acc.values, df_spsa1[df_spsa1.beta_acc>0.6].beta_inf_time.values
+	#acc_beta1, inf_time_beta1 = df_spsa1[df_spsa1.beta_acc>0.6].beta_acc.values, df_spsa1[df_spsa1.beta_acc>0.6].beta_inf_time.values
 
-	acc_beta, inf_time_beta = np.concatenate((acc_beta, acc_beta1)), np.concatenate((inf_time_beta, inf_time_beta1))
+	#acc_beta, inf_time_beta = np.concatenate((acc_beta, acc_beta1)), np.concatenate((inf_time_beta, inf_time_beta1))
 	
-	print(acc_beta)
+	#print(acc_beta)
 
 	acc_beta_index = np.argsort(inf_time_beta)
 	acc_beta, inf_time_beta	= acc_beta[acc_beta_index], inf_time_beta[acc_beta_index]
-	#acc_beta, inf_time_beta	= sorted(acc_beta), sorted(inf_time_beta)
+	acc_beta, inf_time_beta	= sorted(acc_beta), sorted(inf_time_beta)
 
 	acc_no_calib, inf_time_no_calib = df_no_calib.beta_acc.values, df_no_calib.beta_inf_time.values
 	acc_ts, inf_time_ts = df_ts.beta_acc.values, df_ts.beta_inf_time.values
@@ -39,8 +40,8 @@ def plotBetaTradeOff(args, df_spsa, df_spsa1, df_no_calib, df_ts, threshold, n_b
 def main(args):
 
 
-	resultPath = os.path.join(".", "theoretical_beta_analysis_%s_%s_branches_%s_with_overhead_with_nano_with_test_set_pos_2_review_theo.csv"%(args.model_name, args.n_branches, args.model_id))
-	resultPath1 = os.path.join(".", "theoretical_beta_analysis_%s_%s_branches_%s_with_overhead_with_nano_with_test_set_pos_1_review_theo.csv"%(args.model_name, args.n_branches, args.model_id))
+	resultPath = os.path.join(".", "theoretical_beta_analysis_%s_%s_branches_%s_with_overhead_with_nano_with_test_set_pos_3_review_the_overhead_5.csv"%(args.model_name, args.n_branches, args.model_id))
+	#resultPath1 = os.path.join(".", "theoretical_beta_analysis_%s_%s_branches_%s_with_overhead_with_nano_with_test_set_pos_1_review_theo.csv"%(args.model_name, args.n_branches, args.model_id))
 
 	plotDir = os.path.join(".", "plots_pos_review2")
 
@@ -50,20 +51,21 @@ def main(args):
 	threshold = 0.8
 
 	df = pd.read_csv(resultPath)
-	df1 = pd.read_csv(resultPath1)
+	#df1 = pd.read_csv(resultPath1)
 
 	df_inf_data = df[(df.overhead==args.overhead) & (df.threshold==threshold)]
-	df_inf_data1 = df1[(df1.overhead==args.overhead) & (df1.threshold==threshold)]
+	#df_inf_data1 = df1[(df1.overhead==args.overhead) & (df1.threshold==threshold)]
 
-	print(df.threshold.unique())
+	#print(df.threshold.unique())
 
 	df_spsa, df_no_calib, df_ts = df_inf_data[df_inf_data.calib_mode=="beta_calib"], df_inf_data[df_inf_data.calib_mode=="no_calib"], df_inf_data[df_inf_data.calib_mode=="global_TS"]
 	#df_spsa1 = df_inf_data1[df_inf_data1.calib_mode=="beta_calib"]
-	df_spsa1, df_no_calib1, df_ts1 = df_inf_data1[df_inf_data1.calib_mode=="beta_calib"], df_inf_data1[df_inf_data1.calib_mode=="no_calib"], df_inf_data1[df_inf_data1.calib_mode=="global_TS"]
+	#df_spsa1, df_no_calib1, df_ts1 = df_inf_data1[df_inf_data1.calib_mode=="beta_calib"], df_inf_data1[df_inf_data1.calib_mode=="no_calib"], df_inf_data1[df_inf_data1.calib_mode=="global_TS"]
 
 	plotPath = os.path.join(plotDir, "beta_analysis_%s_branches_threshold_%s_overhead_%s_with_nano"%(args.n_branches, threshold, args.overhead) )
 
-	plotBetaTradeOff(args, df_spsa, df_spsa1, df_no_calib, df_ts, threshold, args.n_branches, args.overhead, plotPath)
+	#plotBetaTradeOff(args, df_spsa, df_spsa1, df_no_calib, df_ts, threshold, args.n_branches, args.overhead, plotPath)
+	plotBetaTradeOff(args, df_spsa, df_no_calib, df_ts, threshold, args.n_branches, args.overhead, plotPath)
 
 
 if (__name__ == "__main__"):
