0f0fb4cf6e1bf406c20c30844df60748746f130f
pachecobeto95 <robertopvc@gmail.com>
2023-08-15 21:39:59 -0300

    a

--- run_multiple_theoretical_beta_analysis.sh
+++ run_multiple_theoretical_beta_analysis.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 0 --overhead 15 &
-nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 1 --overhead 15 &
-nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 2 --overhead 15 &
-nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 3 --overhead 15 &
-nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 4 --overhead 15 &
-nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 5 --overhead 15 &
-nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 6 --overhead 15 &
-nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 7 --overhead 15 &
-nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 8 --overhead 15 &
-nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 9 --overhead 15 &
+nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 0 --overhead 7 &
+nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 1 --overhead 7 &
+nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 2 --overhead 7 &
+nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 3 --overhead 7 &
+nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 4 --overhead 7 &
+nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 5 --overhead 7 &
+nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 6 --overhead 7 &
+nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 7 --overhead 7 &
+nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 8 --overhead 7 &
+nohup python3 theoretical_beta_analysis.py --model_name mobilenet --n_branches 3 --model_id 1 --max_iter 20 --theo_data 1 --slot_beta 9 --overhead 7 &
