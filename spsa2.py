807859e055694cfcc6ee01b44138d98aa85514ef
pachecobeto95 <robertopvc@gmail.com>
2023-08-17 19:45:12 -0300

    a

--- spsa2.py
+++ spsa2.py
@@ -346,11 +346,8 @@ def compute_cond_prob(df, temp_list, threshold, idx_branch, current_conf):
 		previous_exit_prob = num_exit_branch/len(df)
 
 	data_conf = df_branch["conf_branch_%s"%(idx_branch+1)].values/temp_list[idx_branch]
-	print(type(data_conf))
 	data_conf = np.array(random.sample(list(data_conf), 100))
-	print(type(data_conf))
 	data_conf = data_conf[:, np.newaxis]
-	sys.exit()
 
 	model = KernelDensity(kernel='gaussian', bandwidth=0.1)
 		#print(len(data_conf))
