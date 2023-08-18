3487ffa96a31cc02e85dd7ff186ec8898533312d
pachecobeto95 <robertopvc@gmail.com>
2023-08-18 14:33:21 -0300

    a

--- spsa.py
+++ spsa.py
@@ -5,6 +5,7 @@ import config
 import pandas as pd
 from sklearn.neighbors import KernelDensity
 from scipy.stats import norm, gaussian_kde
+import matplot.pyplot as plt
 
 class Bernoulli(object):
 	'''
