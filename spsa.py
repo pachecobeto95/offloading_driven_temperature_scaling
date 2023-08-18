ef357642c2eab0e7f6164f5d1c76523d6dbf8444
pachecobeto95 <robertopvc@gmail.com>
2023-08-18 15:01:17 -0300

    a

--- spsa.py
+++ spsa.py
@@ -563,11 +563,13 @@ def compute_theoretical_edge_prob(temp_list, n_branches, threshold, df):
 
 	n_samples = len(df)
 
-	confs = df["conf_branch_%s"%(n_branches)]
-	calib_confs = confs/temp_list[n_branches-1]
-	early_exit_samples = calib_confs >= threshold
+	numexits = df[(df["conf_branch_3"]/temp_list[2] >= threshold) | (df["conf_branch_2"]/temp_list[1] >= threshold) ]["conf_branch_3"].count()
 
-	numexits = df[early_exit_samples]["conf_branch_%s"%(n_branches)].count()
+	#confs = df["conf_branch_%s"%(n_branches)]
+	#calib_confs = confs/temp_list[n_branches-1]
+	#early_exit_samples = calib_confs >= threshold
+
+	#numexits = df[early_exit_samples]["conf_branch_%s"%(n_branches)].count()
 
 	prob = numexits/n_samples
 
