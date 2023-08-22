00cee6b146dc1ab213f02f73bbb6a4cfd15056c4
pachecobeto95 <robertopvc@gmail.com>
2023-08-22 16:53:07 -0300

    a

--- spsa3.py
+++ spsa3.py
@@ -297,6 +297,8 @@ def compute_pdf_values(temp_list, idx_branch, threshold, df):
 	conf_branch, _ = get_confidences(logit_branch, idx_branch, temp_list)
 	pdf, bin_bounds = np.histogram(conf_branch, bins=100, density=True)
 
+	pdf = np.append(pdf, [0])
+
 	for conf in d_confs:
 		n_bin = np.digitize(conf, bin_bounds, right=True)
 		print(conf)
