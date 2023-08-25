7ede753375c79faf70f1ffa38615ba2f30c78018
pachecobeto95 <robertopvc@gmail.com>
2023-08-25 16:55:28 -0300

    a

--- spsa2.py
+++ spsa2.py
@@ -256,7 +256,7 @@ def theoretical_accuracy_edge(temp_list, n_branches, threshold, df):
 		
 		numexits[i] = df_branch["conf_branch_%s"%(i+1)].count()
 		correct_list[i] = df_branch["correct_branch_%s"%(i+1)].sum()
-		p = 1 - (numexits[i]/n_samples)
+		p = 1 - (sum(numexits[:i])/n_samples)
 
 		acc_device[i] = correct_list[i]/numexits[i]
 		theo_acc_device[i] = estimate_expectation(df_branch, i, threshold, temp_list) 
