c40b4974ffad88bb168eb4bf65223d1068d31d41
pachecobeto95 <robertopvc@gmail.com>
2023-08-24 23:08:14 -0300

    a

--- spsa.py
+++ spsa.py
@@ -263,6 +263,7 @@ def theoretical_beta_function(temp_list, n_branches, max_exits, threshold, df, d
 
 	#f = (1-beta)*inf_time_current - beta*acc_current
 	f = inf_time_current - beta*acc_current
+	print(accuracy_edge(temp_list, n_branches, threshold, df), acc_current, temp_list)
 
 	return f, ee_prob
 
