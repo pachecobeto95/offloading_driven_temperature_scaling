ce0ce64bd30c0fdd06c8f5888d32c31030dc93f6
pachecobeto95 <robertopvc@gmail.com>
2023-08-28 13:07:16 -0300

    a

--- spsa2.py
+++ spsa2.py
@@ -258,8 +258,7 @@ def theoretical_accuracy_edge(temp_list, n_branches, threshold, df):
 		correct_list[i] = df_branch["correct_branch_%s"%(i+1)].sum()
 
 		p = 1 - (numexits[i]/n_samples)
-		p = 1 - (sum(numexits[:i])/n_samples)
-
+		
 		acc_device[i] = correct_list[i]/numexits[i]
 		theo_acc_device[i] = estimate_expectation(df, df_branch, p, i, threshold, temp_list) 
 		
@@ -307,9 +306,9 @@ def estimate_expectation(df, df_branch, p, idx_branch, threshold, temp_list, n_b
 	correct = df_branch["correct_branch_%s"%(idx_branch+1)].values
 
 	for i, (bin_lower, bin_upper, pdf) in enumerate(zip(bin_lowers, bin_uppers, pdf_values_full)):
-		in_bin = np.where((conf_branch > bin_lower) & (conf_branch <= bin_upper), True, False)
+		in_bin = np.where((conf_branch_full > bin_lower) & (conf_branch_full <= bin_upper), True, False)
 		prop_in_bin = np.mean(in_bin)
-		confs_in_bin, correct_in_bin = conf_branch[in_bin], correct[in_bin] 
+		confs_in_bin, correct_in_bin = conf_branch_full[in_bin], correct[in_bin] 
 		avg_confs_in_bin = np.mean(confs_in_bin) if (len(confs_in_bin)>0) else 0
 		avg_acc_in_bin = np.mean(correct_in_bin) if (len(correct_in_bin)>0) else 0
 		acc_list.append(avg_confs_in_bin), prop_in_bin_list.append(p*pdf)
