bf37a5d18dbc392746a5bc57ad5323e0a0ac9c20
pachecobeto95 <robertopvc@gmail.com>
2023-08-29 12:09:53 -0300

    a

--- spsa2.py
+++ spsa2.py
@@ -325,7 +325,7 @@ def estimate_expectation(df, df_branch, p, idx_branch, threshold, temp_list, n_b
 
 
 	product = np.array(acc_list)*np.array(prop_in_bin_list)
-	conf_diff = np.diff(bin_boundaries)
+	conf_diff = np.diff(bounds)
 	integral = sum(product*conf_diff)
 	integral2 = np.trapz(product, bin_boundaries[:-1], axis=0)
 	#print(sum(np.array(avg_confs_in_bin)*np.array(prop_in_bin_list)))
