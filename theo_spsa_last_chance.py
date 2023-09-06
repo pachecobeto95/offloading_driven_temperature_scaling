5298b2078a1f8f977df93bed329b34c24e1e0506
pachecobeto95 <robertopvc@gmail.com>
2023-09-06 20:28:44 -0300

    a

--- theo_spsa_last_chance.py
+++ theo_spsa_last_chance.py
@@ -64,11 +64,12 @@ def main(args):
 
 	inf_data_cloud_path = os.path.join(config.DIR_NAME, "last_chance_inf_data_%s_%s_branches.csv"%(args.model_name, args.n_branches))
 	inf_data_device_path = os.path.join(config.DIR_NAME, "new_inference_data", "inference_data_%s_%s_branches_%s_jetson_nano.csv"%(args.model_name, args.n_branches, args.model_id))
-	resultsPath = os.path.join(config.DIR_NAME, "last_chance_theo_beta_analysis_%s_%s_branches_overhead_%s.csv"%(args.model_name, args.n_branches, args.overhead))
+	#resultsPath = os.path.join(config.DIR_NAME, "last_chance_theo_beta_analysis_%s_%s_branches_overhead_%s.csv"%(args.model_name, args.n_branches, args.overhead))
+	resultsPath = os.path.join(config.DIR_NAME, "theo_beta_analysis_%s_%s_branches_overhead_%s_final_rodrigo_version.csv"%(args.model_name, args.n_branches, args.overhead))
+
 
 	threshold_list = [0.8]
-	#beta_list = np.arange(0, 105, 5)	
-	beta_list = [1000]
+	beta_list = np.arange(0, 105, 5)	
 	
 	#beta_list = beta_list[args.slot_beta]
 
