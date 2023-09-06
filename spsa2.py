ee8b0575a8386535ffdf6d1da4ea2df810580e5e
pachecobeto95 <robertopvc@gmail.com>
2023-09-06 15:33:10 -0300

    a

--- spsa2.py
+++ spsa2.py
@@ -313,8 +313,10 @@ def estimate_expectation(df, df_branch, p, idx_branch, threshold, temp_list, n_b
 
 	pdf_values, b = np.histogram(conf_branch, bins=n_bins, density=True)
 
-	b = b[b >= threshold]
-	pdf_values = pdf_values[b >= threshold]
+	update_bin = b >= threshold
+
+	b = b[update_bin]
+	pdf_values = pdf_values[update_bin]
 	#update_bin = np.where(b >= threshold, True, False)
 	#b = b[update_bin]
 
