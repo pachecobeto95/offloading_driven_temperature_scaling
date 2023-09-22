7b1db59f87f70a1feebabe01822464b04dd67f1c
pachecobeto95 <robertopvc@gmail.com>
2023-09-22 15:50:50 -0300

    a

--- theo_spsa_last_chance.py
+++ theo_spsa_last_chance.py
@@ -86,13 +86,13 @@ def main(args):
 
 
 	#The next row specifies the file name that saves the results. 	
-	resultsPath = os.path.join(config.DIR_NAME, "theo_beta_analysis_%s_%s_branches_overhead_%s_rodrigo_version_test_2.csv"%(args.model_name, args.n_branches, args.overhead))
+	resultsPath = os.path.join(config.DIR_NAME, "theo_beta_analysis_%s_%s_branches_overhead_%s_rodrigo_version_test_final_1.csv"%(args.model_name, args.n_branches, args.overhead))
 
 	threshold_list = [0.8]
 
 	#Defines a list of beta to evaluate the optimization problem. 
-	beta_list = np.arange(0, 202, 2)
-	beta_list = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]	
+	beta_list = np.arange(0, 102, 2)
+	#beta_list = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]	
 	
 	#The next rows reads the inference data 
 	df_inf_data_cloud = pd.read_csv(inf_data_cloud_path)
