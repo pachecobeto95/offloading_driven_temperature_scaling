e9d3b8a609e505ac0151a8d57a888d65c203d2b0
pachecobeto95 <robertopvc@gmail.com>
2023-09-22 13:56:14 -0300

    a

--- spsa2.py
+++ spsa2.py
@@ -207,12 +207,12 @@ class SPSA (object):
 			#idx_k = np.argmin(y_alt_list)
 			#y_k, theta = y_alt_list[idx_k], theta_alt_list[idx_k]
 
-			print("Iter: %s, Function: %s, ACC: %s, Inf Time: %s"%(k, y_k, acc_k, inf_time_k))
+			print("Iter: %s, Function: %s, ACC: %s, Inf Time: %s, ACC EXP: %s"%(k, y_k, acc_k, inf_time_k. acc_exp_k))
 
 
 			if (y_k < best_loss):
 				print("UPDATEE")
-				print("Iter: %s, Function: %s, ACC: %s, InfTime: %s"%(k, y_k, acc_k, inf_time_k))
+				print("Iter: %s, Function: %s, ACC: %s, InfTime: %s, ACCEXP: %s"%(k, y_k, acc_k, inf_time_k, acc_exp_k))
 
 				best_loss = y_k
 				best_theta = copy.copy(theta)
