01a38b4469b4fdef1731ec439debc7b34f9a32f9
pachecobeto95 <robertopvc@gmail.com>
2023-09-26 13:45:56 -0300

    a

--- theoretical_beta_analysis.py
+++ theoretical_beta_analysis.py
@@ -135,8 +135,7 @@ def main(args):
 	#resultsPath = os.path.join(config.DIR_NAME, "theoretical_beta_analysis_%s_%s_branches_%s_with_overhead_with_nano_with_test_set_pos_6_review_%s_overhead_%s.csv"%(args.model_name, args.n_branches, args.model_id, mode, args.overhead))
 	resultsPath = os.path.join(config.DIR_NAME, "theo_concorrents_beta_analysis_%s_%s_branches_overhead_%s.csv"%(args.model_name, args.n_branches, args.overhead))
 
-
-	global_ts_path = os.path.join(config.DIR_NAME, "alternative_temperature_%s_%s_branches_id_%s.csv"%(args.model_name, args.n_branches, args.model_id))
+	global_ts_path = os.path.join(config.DIR_NAME, "alternative_temperature_%s_%s_branches_id_%s_rodrigo_version.csv"%(args.model_name, args.n_branches, args.model_id))
 
 	threshold_list = [0.7, 0.8, 0.85]
 	#beta_list = np.arange(0, config.max_beta+config.step_beta, 0.1)
